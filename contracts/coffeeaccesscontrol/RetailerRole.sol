ad74a26d14f44e4e2864cb7c933f138d485ff55e
cenwadike <chinedunwadike04@gmailcom>
2022-05-26 13:14:18 +0100

    Role rewrite

--- contracts/coffeeaccesscontrol/RetailerRole.sol
+++ contracts/coffeeaccesscontrol/RetailerRole.sol
@@ -1,54 +1,45 @@
+// SPDX-License-Identifier: UNLICENCED
+
 pragma solidity ^0.4.24;
 
-// Import the library 'Roles'
 import "./Roles.sol";
 
-// Define a contract 'RetailerRole' to manage this role - add, remove, check
 contract RetailerRole {
-  using Roles for Roles.Role;
+    using Roles for Roles.Role;
 
-  // Define 2 events, one for Adding, and other for Removing
-  event RetailerAdded(address indexed account);
-  event RetailerRemoved(address indexed account);
+    Roles.Role private retailers;
 
-  // Define a struct 'retailers' by inheriting from 'Roles' library, struct Role
-  Roles.Role private retailers;
-  
-  // In the constructor make the address that deploys this contract the 1st retailer
-  constructor() public {
-    _addRetailer(msg.sender);
-  }
+    event RetailerAdded(address indexed account);
+    event RetailerRemoved(address indexed account);
 
-  // Define a modifier that checks to see if msg.sender has the appropriate role
-  modifier onlyRetailer() {
-    require(isRetailer(msg.sender));
-    _;
-  }
+    modifier onlyRetailer() {
+        require(isRetailer(msg.sender), "INVALID: Not a Retailer");
+        _;
+    }
 
-  // Define a function 'isRetailer' to check this role
-  function isRetailer(address account) public view returns (bool) {
-    return retailers.has(account);
-  }
+    constructor() public {
+        _addRetailer(msg.sender);
+    }
 
-  // Define a function 'addRetailer' that adds this role
-  function addRetailer(address account) public onlyRetailer {
-    _addRetailer(account);
-  }
+    function isRetailer(address account) public view returns (bool) {
+        return retailers.has(account);
+    }
 
-  // Define a function 'renounceRetailer' to renounce this role
-  function renounceRetailer() public {
-    _removeRetailer(msg.sender);
-  }
+    function addRetailer(address account) public onlyRetailer {
+        _addRetailer(account);
+    }
 
-  // Define an internal function '_addRetailer' to add this role, called by 'addRetailer'
-  function _addRetailer(address account) internal {
-    retailers.add(account);
-    emit RetailerAdded(account);
-  }
+    function renounceRetailer() public {
+        _removeRetailer(msg.sender);
+    }
 
-  // Define an internal function '_removeRetailer' to remove this role, called by 'removeRetailer'
-  function _removeRetailer(address account) internal {
-    retailers.remove(account);
-    emit RetailerRemoved(account);
-  }
+    function _addRetailer(address account) internal {
+        retailers.add(account);
+        emit RetailerAdded(account);
+    }
+
+    function _removeRetailer(address account) internal {
+        retailers.remove(account);
+        emit RetailerRemoved(account);
+    }
 }
--- contracts/coffeeaccesscontrol/Roles.sol
+++ contracts/coffeeaccesscontrol/Roles.sol
@@ -1,44 +1,37 @@
+// SPDX-License-Identifier: UNLICENCED
+
 pragma solidity ^0.4.24;
 
 /**
  * @title Roles
  * @dev Library for managing addresses assigned to a Role.
  */
+
 library Roles {
   struct Role {
     mapping (address => bool) bearer;
   }
 
-  /**
-   * @dev give an account access to this role
-   */
   function add(Role storage role, address account) internal {
-    require(account != address(0));
-    require(!has(role, account));
+    require(account != address(0), "INVALID: DEAD Address");
+    require(!has(role, account), "INVALID: Account Have Role");
 
     role.bearer[account] = true;
   }
 
-  /**
-   * @dev remove an account's access to this role
-   */
   function remove(Role storage role, address account) internal {
-    require(account != address(0));
-    require(has(role, account));
+    require(account != address(0), "INVALID: DEAD Address");
+    require(has(role, account), "INVALID: Account Have No Role");
 
     role.bearer[account] = false;
   }
 
-  /**
-   * @dev check if an account has this role
-   * @return bool
-   */
   function has(Role storage role, address account)
     internal
     view
     returns (bool)
   {
-    require(account != address(0));
+    require(account != address(0), "INVALID: DEAD Address");
     return role.bearer[account];
   }
 }
